9d6b3962ea76f3c246f58b8dafaaef55cccb412b
Oyeniji <nijoskysadeeq@gmail.com>
2023-10-14 18:15:39 +0100

    minor changes
    	modified:   src/App.js
    	modified:   src/Pages/Home.js
    	modified:   src/assets/main.css
    	modified:   src/components/Banner/DesignBanner.js
    	modified:   src/components/CustomNavbar.js
    	modified:   src/components/Subscribe.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import CustomNavbar from '../components/CustomNavbar';
-import DesignBanner from '../components/Banner/DesignBanner';
-import Service from '../components/Service/Service';
-import Subscribe from '../components/Subscribe';
-import Footer from '../components/Footer/Footer';
-import FooterData from '../components/Footer/FooterData';
+import React, { useState } from "react";
+import CustomNavbar from "../components/CustomNavbar";
+import DesignBanner from "../components/Banner/DesignBanner";
+import Service from "../components/Service/Service";
+import Subscribe from "../components/Subscribe";
+import Footer from "../components/Footer/Footer";
+import FooterData from "../components/Footer/FooterData";
 
-export const Home = () => (
+const Home = () => {
+  const [isHovered, setIsHovered] = useState(false);
+  return (
     <div className="body_wrapper">
-        <CustomNavbar cClass="custom_container p0" hbtnClass="new_btn"/>
-        <DesignBanner/>
-        <Service/>
-        <Subscribe FooterData={FooterData}/>
-        <Footer FooterData={FooterData}/>
+      <CustomNavbar cClass="custom_container p0" hbtnClass="new_btn" />
+      <DesignBanner />
+      <div
+        className="section"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        {isHovered && (
+          <div className="overlay">
+            <p>Coming Soon</p>
+          </div>
+        )}
+        <Service />
+      </div>
+      <div
+        className="section"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        {isHovered && (
+          <div className="overlay">
+            <p>Coming Soon</p>
+          </div>
+        )}
+        <Subscribe FooterData={FooterData} />
+      </div>
+      <div
+        className="section"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        {isHovered && (
+          <div className="overlay">
+            <p>Coming Soon</p>
+          </div>
+        )}
+        <Footer FooterData={FooterData} />
+      </div>
     </div>
-)
+  );
+};
+export default Home;
